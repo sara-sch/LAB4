79ab224b00b4ce04e4e46ffb8bd8acccb19a23b0
sara-sch <99369223+sara-sch@users.noreply.github.com>
2022-02-19 13:18:48 -0600

    Lab4 - Final con anotaciones

--- mainl4.s
+++ mainl4.s
@@ -26,13 +26,13 @@ PROCESSOR 16F887
 PSECT udata_shr			 ; Memoria compartida
     W_TEMP:		DS 1
     STATUS_TEMP:	DS 1
-    COUNT:		DS 1
-    CONT:		DS 1	; Contador tabla
-    COUNT2:		DS 1
-    CONT2:		DS 1	; Contador tabla
+    COUNT:		DS 1	; Contador primer display
+    CONT:		DS 1	; Contador interno tabla primer display
+    COUNT2:		DS 1	; Contador segundo display
+    CONT2:		DS 1	; Contador interno tabla segundo display
      
 PSECT resVect, class = CODE, abs, delta = 2
-; ----------- VECTOR RESET ----------------
+; -------------------- VECTOR RESET -----------------------
  
 ORG 00h
 resVect:
@@ -48,10 +48,10 @@ PUSH:
     MOVWF   STATUS_TEMP	    ; Guardamos STATUS
     
 ISR:  
-    BTFSC   RBIF
-    CALL    INT_IOCB
-    CALL    RESET_TMR0
-    CALL    COUNTER
+    BTFSC   RBIF	    ; Interrupción del PORTB
+    CALL    INT_IOCB	    ; Subrutina de interrupción de PORTB
+    CALL    RESET_TMR0	    ; Reset del TMR0
+    CALL    COUNTER	    ; Contadores
     
 POP:
     SWAPF   STATUS_TEMP, W
@@ -60,35 +60,34 @@ POP:
     SWAPF   W_TEMP, W
     RETFIE
 
-;----------------subrutinas int---------------------    
+;---------------------subrutinas de int--------------------    
 INT_IOCB:
     BANKSEL PORTA
-    BTFSS   PORTB, 0
-    INCF    PORTA
-    BTFSS   PORTB, 1
-    DECF    PORTA
-    BCF	    RBIF
+    BTFSS   PORTB, 0	; Primer botón
+    INCF    PORTA	; Incremento de contador
+    BTFSS   PORTB, 1	; Segundo botón
+    DECF    PORTA	; Decremento de contador
+    BCF	    RBIF	; Se limpia bandera de interrupción de PORTB
     RETURN
     
-    
-    
 PSECT code, delta = 2, abs
-; ----------- CONFIGURATION ---------------
+ 
+; -------------------- CONFIGURATION ---------------------
 ORG 100h
 main:
-    CALL CONFIG_IO
+    CALL CONFIG_IO	; Configuraciones para que el programa funcione correctamente
     CALL CONFIG_RELOJ
     CALL CONFIG_IOCB
     CALL CONFIG_INT
     CALL CONFIG_TMR0
-    CLRF CONT			; Reinicio de contador para tabla
-    CLRF CONT2
+    CLRF CONT		; Reinicio de contador para tabla del display 1
+    CLRF CONT2		; Reinicio de contador para tabla del display 2
     BANKSEL PORTA
         
 LOOP:
     GOTO LOOP
     
-; ------------subrutinas
+; ----------------------subrutinas------------------------
 
 CONFIG_IO:
     BANKSEL ANSEL
@@ -96,24 +95,24 @@ CONFIG_IO:
     CLRF    ANSELH		; I/O digitales
     
     BANKSEL TRISA
-    BSF	    TRISB, 0		; PORTB0 como entrada
-    BSF	    TRISB, 1		; PORTB1 como entrada
+    BSF	    TRISB, 0		; PORTB como entrada
+    BSF	    TRISB, 1		
     BCF	    TRISA, 0		; PORTA como salida
     BCF	    TRISA, 1
     BCF	    TRISA, 2
     BCF	    TRISA, 3
     CLRF    TRISC		; PORTC como salida
-    CLRF    TRISD
+    CLRF    TRISD		; PORTD como salida
     
     BANKSEL OPTION_REG
-    BCF	    OPTION_REG, 7
+    BCF	    OPTION_REG, 7	; PORTB Pull-up habilitado
     
     BANKSEL WPUB
-    BSF	    WPUB, 0
-    BSF	    WPUB, 1
+    BSF	    WPUB, 0		; PORTB0 habilitado como Pull-up
+    BSF	    WPUB, 1		; PORTB1 habilitado como Pull-up
     
     BANKSEL PORTA
-    CLRF    PORTA
+    CLRF    PORTA		; Limpieza de puertos
     CLRF    PORTB
     CLRF    PORTC
     CLRF    PORTD
@@ -121,7 +120,7 @@ CONFIG_IO:
     RETURN
     
 CONFIG_RELOJ:
-    BANKSEL OSCCON	;cambiamos a banco 1
+    BANKSEL OSCCON	; Cambiamos a banco 1
     BSF OSCCON, 0	; scs -> 1, usamos reloj interno
     BSF OSCCON, 6
     BSF OSCCON, 5
@@ -129,17 +128,17 @@ CONFIG_RELOJ:
     RETURN
     
 CONFIG_TMR0:
-    BANKSEL OPTION_REG	; cambiamos de banco
+    BANKSEL OPTION_REG	; Cambiamos de banco
     BCF T0CS		; TMR0 como temporizador
-    BCF PSA		; prescaler a TMR0
+    BCF PSA		; Prescaler a TMR0
     BSF PS2
     BSF PS1
-    BCF PS0		; PS<2:0> -> 111 PRESCALER 1 : 128
+    BCF PS0		; PS<2:0> -> 110 PRESCALER 1 : 128
     
-    BANKSEL TMR0	; cambiamos de banco
+    BANKSEL TMR0	; Cambiamos de banco
     MOVLW 61
     MOVWF TMR0		; 20ms retardo
-    BCF T0IF		; limpiamos bandera de interrupci[on
+    BCF T0IF		; Limpiamos bandera de interrupción
     RETURN
  
 RESET_TMR0:
@@ -160,59 +159,60 @@ CONFIG_INT:
     
 CONFIG_IOCB:
     BANKSEL TRISA
-    BSF	    IOCB, 0
-    BSF	    IOCB, 1
+    BSF	    IOCB, 0	; Interrupción habilitada en PORTB0
+    BSF	    IOCB, 1	; Interrupción habilitada en PORTB1
     
     BANKSEL PORTB
     MOVF    PORTB, W	; Al leer, deja de hacer mismatch
-    BCF	    RBIF
+    BCF	    RBIF	; Limpiamos bandera de interrupción
     RETURN
     
 COUNTER:
-    INCF    COUNT
-    MOVLW   50
-    XORWF   COUNT, W
-    BTFSS   STATUS, 2
+    INCF    COUNT		; Incremento del contador de display 1
+    MOVLW   50			
+    XORWF   COUNT, W		
+    BTFSS   STATUS, 2		; Si se activa la bandera Z -- Después de 1s
     RETURN
-    MOVF    CONT, W		; Valor de contador a W para buscarlo en la tabla
+    MOVF    CONT, W		; Valor de contador interno a W para buscarlo en la tabla
     CALL    TABLA		; Buscamos caracter de CONT en la tabla ASCII
-    MOVWF   PORTC
-    INCF    CONT		; Incremento de contador
+    MOVWF   PORTC		; Se muestra resultado en PORTD
+    INCF    CONT		; Incremento del contador interno
     
-    CLRW
+    CLRW			; Limpiamos W
     MOVLW   11
     XORWF   CONT, W
-    BTFSC   STATUS, 2
-    CALL    COUNTERD2
+    BTFSC   STATUS, 2		; Si se activa la bandera Z -- Después de 10s
+    CALL    COUNTERD2		; Subrutina del segundo display
    
-    CLRF    STATUS
+    CLRF    STATUS		; Limpiamos bandera STATUS
     CLRF    COUNT
     RETURN
     
 COUNTERD2:
-    CALL    RESET_TMR0
-    CLRF    CONT
+    CALL    RESET_TMR0		; Reinicio de TMR0
+    CLRF    CONT		; Limpiamos contador interno de display 1
     
-    INCF    COUNT2
+    
+    INCF    COUNT2		; Incremento del contador de display 2
     MOVF    CONT2, W		; Valor de contador a W para buscarlo en la tabla
     CALL    TABLA		; Buscamos caracter de CONT en la tabla ASCII
-    MOVWF   PORTD
-    INCF    CONT2
+    MOVWF   PORTD		; Se muestra resultado en PORTD
+    INCF    CONT2		; Incremento del contador interno
     
-    CLRW
-    MOVLW   4
+    CLRW			; Limpiamos W
+    MOVLW   6
     XORWF   COUNT2, W
-    BTFSC   STATUS, 2
-    CALL    RESETCNTS
+    BTFSC   STATUS, 2		; Si se activa la bandera Z -- Después de 60s
+    CALL    RESETCNTS		; Subrutina de resets de contadores
     RETURN
     
 RESETCNTS:
-    CALL    RESET_TMR0
-    CLRF    CONT
+    CALL    RESET_TMR0		; Reinicio de TMR0
+    CLRF    CONT		; Limpiamos contadores de ambos displays
     CLRF    CONT2
     CLRF    COUNT
     CLRF    COUNT2
-    CLRF    STATUS
+    CLRF    STATUS		; Limpiamos bandera STATUS
     RETURN
     
     
